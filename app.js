debfe621c69c3aa6f6e7be0f56efe9c30cc74429
Brian Crowe <brian.j.crowe@protonmail.com>
2025-10-03 21:05:30 -0400

    update global error handler to take advantage of validation

--- app.js
+++ app.js
@@ -42,13 +42,21 @@ app.use((req, res) => {
 // Setup a global error handler
 app.use((err, req, res, next) => {
   if (enableGlobalErrorLogging) {
-    console.error(`Global error handler: ${JSON.stringify(err.stack)}`)
+    console.error(`Global error handler: ${err.stack}`)
   }
 
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: {}
-  })
+  if (
+    err.name === 'SequelizeValidationError' ||
+    err.name === 'SequelizeUniqueConstraintError'
+  ) {
+    const errors = err.errors.map((e) => e.message)
+    res.status(400).json({ errors })
+  } else {
+    res.status(err.status || 500).json({
+      message: err.message,
+      error: {}
+    })
+  }
 })
 
 // Set our port
